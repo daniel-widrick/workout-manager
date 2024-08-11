3f016bce475fba6f7bf0e189345b79f62df1d96a
lvlint67 <daniel@widrick.net>
2024-08-11 12:53:42 -0400

    Add highlight suport for current set

--- frontend/timerRun.js
+++ frontend/timerRun.js
@@ -1,4 +1,5 @@
 let currentSet = {}
+window.TimerRun = true
 
 export const processRoutine = async function(routine){
 	console.log(routine)
@@ -16,11 +17,17 @@ export const processRoutine = async function(routine){
 async function handleSet(set){
 	return new Promise(resolve => {
 		const interval = setInterval(() => {
+			set.active="active"
+			if(!window.TimerRun){
+				return
+			}
 			console.log(`${set.type}: ${set.length} seconds remaining`)
+			window.statusMessage.value = `${set.type} for ${ new Date(set.length * 1000).toISOString().slice(11, 19)}`
 			set.length--
 			if(set.length <= 0) {
 				clearInterval(interval)
 				set.length = currentSet.length
+				set.active="done"
 				resolve()
 			}
 		}, 1000)
@@ -29,12 +36,18 @@ async function handleSet(set){
 
 async function handleInterval(interval){
 	//console.log("Interval: ",interval)
+	interval.active="intervalActive"
 	for(let i = 0; i < interval.repeat; interval.repeat--){
 		console.log("---Interval Start---")
+		//Reset sets to not done
+		for(let set of interval.routine){
+			set.active=""
+		}
 		for(let set of interval.routine){
 			currentSet = JSON.parse(JSON.stringify(set))
 			await handleSet(set)
 		}
 		console.log("---Interval End---")
 	}
+	interval.active="done"
 }
