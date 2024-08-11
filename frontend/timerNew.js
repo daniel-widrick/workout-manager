2c49106c587898ff7cc5233656798be9204795b3
lvlint67 <daniel@widrick.net>
2024-08-11 12:44:44 -0400

    Disable start while running and add status message

--- frontend/timerNew.js
+++ frontend/timerNew.js
@@ -8,20 +8,36 @@ createApp({
 		const message = ref('Hello World!')
 		const workoutData = ref(null)
 		const isLoading = ref(true)
+		const startEnabled = ref(true)
+		const statusMessage = ref("")
+		window.statusMessage = statusMessage
+		let workoutDataReset = {}
+		let oldMessage = ""
 
 		const workoutFetch = async() => {
 			workoutData.value = await fetchWorkout()
 			console.log("Data Fetched:", workoutData.value)
 			//processRoutine(workoutData.value)
 			isLoading.value = false
+			workoutDataReset = JSON.parse(JSON.stringify(workoutData.value))
 		}
 		const start = async(event) => {
 			event.stopPropagation()
 			window.TimerRun=true
+			startEnabled.value=false
 			await processRoutine(workoutData.value)
+			startEnabled.value=true
+			workoutData.value = JSON.parse(JSON.stringify(workoutDataReset))
+			window.statusMessage.value = "COMPLETE!"
 		}
 		const pauseToggle = async() => {
 			window.TimerRun=(!window.TimerRun)
+			if(!window.TimerRun){
+				oldMessage = statusMessage.value
+				statusMessage.value = "PAUSED"
+			} else {
+				statusMessage.value = oldMessage
+			}
 		}
 
 		//Fetch Workout Data
@@ -37,7 +53,7 @@ createApp({
 
 		//export
 		return {
-			message, workoutData, isLoading, start
+			message, workoutData, isLoading, start, startEnabled, statusMessage
 		}
 		
 	}
